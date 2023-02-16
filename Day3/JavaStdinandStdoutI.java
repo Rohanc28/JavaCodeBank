b9ccdb4208ebf946dfeda95ec6ef7713a35a7e2f
Rohanc28 <rochaturvedi01@gmail.com>
2023-02-16 12:31:17 +0530

    16

---
+++ Day3/JavaStdinandStdoutI.java
@@ -0,0 +1,16 @@
+//https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?h_r=profile
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        // Complete this line
+        // Complete this line
+    }
+}

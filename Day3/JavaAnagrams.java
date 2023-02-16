a407c2c98f8e937ae5d2b71732b6f2fefbf1f914
Rohanc28 <rochaturvedi01@gmail.com>
2023-02-16 12:16:45 +0530

    JavaAnagrams

---
+++ Day3/JavaAnagrams.java
@@ -0,0 +1,23 @@
+//https://www.hackerrank.com/challenges/java-anagrams/submissions/code/314504082
+static boolean isAnagram(String a, String b) {  if (a.length() != b.length()) {
+            return false;
+        }
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        int sum = 0;
+        for (char c = 'a'; c <= 'z'; c++) {
+            for (int i=0; i<a.length(); i++) {
+                if (a.charAt(i) == c) {
+                    sum++;
+                }
+                if (b.charAt(i) == c) {
+                    sum--;
+                }
+            }
+            if (sum != 0) {
+                return false;
+            }
+        }
+        return true;
+        
+}

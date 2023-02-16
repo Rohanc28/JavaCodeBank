7a09ed76ac9881021365515ef9c0538b1b67879f
Rohanc28 <rochaturvedi01@gmail.com>
2023-02-16 12:33:02 +0530

    18

---
+++ Day3/HelloJava.java
@@ -0,0 +1,6 @@
+//https://www.hackerrank.com/challenges/welcome-to-java/problem?h_r=profile
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.\nHello, Java.");
+    }
+}

6a50b591dea844ae392c8a0a6a82e054f567b5a7
Rohanc28 <rochaturvedi01@gmail.com>
2023-02-17 10:56:19 +0530

    16

--- Day3/JavaStdinandStdoutI.java
+++ Day3/JavaStdinandStdoutI.java
@@ -6,11 +6,8 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
-        // Complete this line
-        // Complete this line
-
-        System.out.println(a);
-        // Complete this line
-        // Complete this line
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        System.out.println(a+"\n"+b+"\n"+c);
     }
 }

d6c13e08891c3275f314329308e71c6346c79c97
Rohanc28 <rochaturvedi01@gmail.com>
2023-02-19 16:20:30 +0530

    5

---
+++ Day4/StackUsingArray.java
@@ -0,0 +1,65 @@
+package Java_Program;
+import java.util.ArrayList;
+public class StackUsingArray {
+
+	public static class Stack {
+
+	public class Stack {
+
+    private int[] stackArray;
+    private int top;
+    private int capacity;
+
+    public Stack(int capacity) {
+        stackArray = new int[capacity];
+        top = -1;
+        this.capacity = capacity;
+    }
+
+    public void push(int element) {
+        if (isFull()) {
+            throw new IllegalStateException("Stack is full");
+        }
+        top++;
+        stackArray[top] = element;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        int lastElement = stackArray[top];
+        top--;
+        return lastElement;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return stackArray[top];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == capacity - 1;
+    }
+}
+
+	public static void main(String[] args) {
+		Stack s = new Stack();
+		s.push(0);
+		s.pop();
+		s.push(10);
+		System.out.println(s.peek());
+		if(!s.isEmpty()) {
+			System.out.println("Not empty");
+		}
+		else{
+			System.out.println("Empty");
+		}
+	}
+}

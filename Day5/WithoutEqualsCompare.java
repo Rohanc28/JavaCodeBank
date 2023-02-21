b5356582e843e70eb9a9f25413bb3db5321f52f3
Rohanc28 <rochaturvedi01@gmail.com>
2023-02-21 17:20:11 +0530

    Q9

---
+++ Day5/WithoutEqualsCompare.java
@@ -0,0 +1,23 @@
+
+import java.util.*; 
+public class WithoutEqualsCompare {
+  
+    static void usingXor(int a, int b){
+        if ((a ^ b) != 0)
+            System.out.print("Not Same");
+        else
+            System.out.print("Same");
+    }
+    static void compareInt(int a,int b){
+        if (a==b){
+            System.out.print("Same");
+        }
+        else
+            System.out.print("Not Same");
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Using Xor: ");usingXor(10, 20);
+        System.out.println("\nUsing comparison: ");compareInt(10, 20);
+    }
+}

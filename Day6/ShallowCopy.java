3670c8caf95918601cf537751a8d3bab5fbaa4f8
Rohanc28 <rochaturvedi01@gmail.com>
2023-02-27 16:07:43 +0530

    ShallowCopy

--- Day6/ShallowCopy.java
+++ Day6/ShallowCopy.java
@@ -11,7 +11,7 @@ public class ShallowCopyExample   {
         ShallowCopy obj2 = obj1;  
   
         obj2.x = 2;  
-  
+        //values are copied as both objects share reference
         System.out.println("The value of x is: " + obj1.x);  
     }  
 }   

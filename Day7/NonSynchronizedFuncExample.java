095001318ccf66bebb8c089417049ac75b5d6dc9
Rohanc28 <rochaturvedi01@gmail.com>
2023-02-28 13:33:35 +0530

    NonSync Prog

---
+++ Day7/NonSynchronizedFuncExample.java
@@ -0,0 +1,49 @@
+package Java_Program;
+import java.util.*;
+
+class PrintString {
+	void printString(String str) {
+		for(int i=0;i<str.length();i++) {
+			System.out.println(str.charAt(i));
+		
+		try {
+			System.out.print(" ...zzZZZ    ");
+			Thread.sleep(400);
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+}
+}
+ class Thread1 extends Thread{
+	PrintString printStrObject1;
+	Thread1(PrintString printStrObject1){
+		this.printStrObject1 = printStrObject1;
+	}
+	public void run() {
+		printStrObject1.printString("abcdefghij");
+	}
+}
+
+ class Thread2 extends Thread{//implements Runnable{
+	PrintString printStrObject2;
+	Thread2(PrintString printStrObject2){
+		this.printStrObject2 = printStrObject2;
+	}
+	public void run() {
+		printStrObject2.printString("ABCDEFGHIJ");
+	}
+}
+
+
+public class NonSynchronizedFuncExample {
+
+	public static void main(String[] args) {
+		PrintString newObj = new PrintString();
+		Thread1 thread1Obj = new Thread1(newObj);
+		Thread2	thread2Obj = new Thread2(newObj);
+		thread1Obj.start();
+		thread2Obj.start();
+	}
+
+}
